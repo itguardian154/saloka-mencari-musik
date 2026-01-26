96fb6b8a52233fe59ccabad95be785c6d3bd64d9
rezaabidin14 <rezaabidin14@gmail.com>
2026-01-26 11:35:22 +0700

    setup staging

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const API_URLS = {
     secretKey: "//FatmaW4T!_154",
-    mencariMusik: "https://servicemencarimusik.salokapark.app/api",
-    //mencariMusik: "http://10.10.100.86:34291/api",
+    // mencariMusik: "http://10.10.100.86:34291/api",
+    mencariMusik: "http://10.10.100.69:8703/api",
     crm: "https://servicecrm.salokapark.app/",
 };
 
